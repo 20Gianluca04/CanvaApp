be1cf95b1927467523b9eecb92a9490a95046376
20Gianluca04 <54109058+20Gianluca04@users.noreply.github.com>
2024-03-04 19:39:24 +0100

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,44 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
+import { Button, Rows, Text, Alert } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
 import * as React from "react";
+import { useSelection } from "utils/use_selection_hook";
 import styles from "styles/components.css";
+import { useState } from "react";
+import Days from "./days"
 
 export const App = () => {
-  const onClick = () => {
-    addNativeElement({
-      type: "TEXT",
-      children: ["Hello world!"],
-    });
+  const [days, setDaysVisible] = useState([]);
+  const [dayCounter, setDayCounter] = useState(0);
+  const selection = useSelection("plaintext");
+
+  const updateDaysVisible = (newDays) => {
+    setDaysVisible(newDays);
+    setDayCounter(0);
+  };
+
+  const updateDayCounter = () =>{
+    if(dayCounter==days.length-1) setDayCounter(0);
+    else setDayCounter(dayCounter+1);
+  };
+
+  const updateText = async (e) => {
+    e.preventDefault();
+    if (selection.count == 0) return;
+      
+    const draft = await selection.read();
+    draft.contents.forEach((s) => (s.text = `${days[dayCounter]}`));
+    updateDayCounter();
+    await draft.save();
   };
 
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
-        <Text>
-          To make changes to this app, edit the{" "}
-          <code className={styles.code}>src/app.tsx</code> file, then close and
-          reopen the app in the editor to preview the changes.
-        </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
+        <Days updateDaysVisible={updateDaysVisible} arrayDays={days}></Days>
+        <Button variant="primary" onClick={updateText} stretch>
+          Sostituisci giorno
         </Button>
+        
       </Rows>
     </div>
   );
